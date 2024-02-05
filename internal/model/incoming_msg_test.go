28349e88ec64ec4a928986e1974edc04e0c9ef43
dokshiye <okshieva@gmail.com>
2024-02-06 01:56:12 +0600

    model

---
+++ internal/model/incoming_msg_test.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func OnStartingCommand(t *testing.T){
+	model := New(nil)
+
+	err := model.IncomingMessage(Message{
+		Text: "/start",
+		UserID: 123,
+	})
+
+	assert.NoError(t, err)
+}
